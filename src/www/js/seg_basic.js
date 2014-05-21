6587ab79935e185f8387f959e6a9516decbfda81
robinisaksson <robin.isaksson@gmail.com>
2014-05-21 16:40:58 +0200

    fast css fixes - build

--- src/www/js/seg_basic.js
+++ src/www/js/seg_basic.js
@@ -3,6 +3,6 @@
 
 /*i-unsupported.js*/
 function unsupported() {
-	document.body.innerHTML = '<h1 style="text-align: center; margin: 20%; font-family: Arial; color: #00027a">We are sorry, but your browser is not supported by this website. Please upgrade.</h1>';
+	document.body.innerHTML = '<h1 style="text-align: center; margin: 20%; font-family: Arial; color: #00027a">We are sorry, but your browser/device is not supported by this website.</h1>';
 }
 window.onload = unsupported;
