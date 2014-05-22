0ffb534e87b9e67ffc80c52c6a937998967f8843
kaestel <kaestel@e-types.com>
2014-05-22 14:45:53 +0200

    updated

--- src/www/js/seg_desktop_ie.js
+++ src/www/js/seg_desktop_ie.js
@@ -3769,11 +3769,11 @@ Util.Objects["signature"] = new function() {
 			this.bn_preview.scene = this;
 			this.bn_preview.onclick = function(event) {
 				u.e.kill(event);
-				if(this.scene.canvas_date.paths.paths.length < 10) {
+				if(this.scene.canvas_date.paths.paths.length < 40) {
 					this.value = "Godkend";
 					this.scene.canvas_date.clicked();
 				}
-				else if(this.scene.canvas_signature.paths.paths.length < 10) {
+				else if(this.scene.canvas_signature.paths.paths.length < 40) {
 					this.value = "Godkend";
 					this.scene.canvas_signature.clicked();
 				}
