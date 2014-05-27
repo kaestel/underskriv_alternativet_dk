7ccb3ecee002f0f9ac17204757da76b888993f44
kaestel <kaestel@e-types.com>
2014-05-27 11:09:15 +0200

    updated

--- src/www/js/seg_tv.js
+++ src/www/js/seg_tv.js
@@ -3,6 +3,6 @@
 
 /*i-unsupported.js*/
 function unsupported() {
-	document.body.innerHTML = '<h1 style="text-align: center; margin: 20%; font-family: Arial; color: #00027a">We are sorry, but your browser/device is not supported by this website.</h1>';
+	document.body.innerHTML = '<h1 style="text-align: center; margin: 20%; font-family: Arial; color: #00027a">Vi beklager, men den digitale vælgererklæring dur ikke på mobiltelefoner og i visse ældre browsere.</h1>';
 }
 window.onload = unsupported;
