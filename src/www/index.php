e0526364a2910c81eb55300773eb9c776fbd8955
kaestel <kaestel@e-types.com>
2015-02-15 19:35:08 +0000

    testing ie11

--- src/www/index.php
+++ src/www/index.php
@@ -4,6 +4,7 @@ if(isset($read_access) && $read_access) {
 	return;
 }
 
+header('P3P: CP="NOI ADM DEV COM NAV OUR STP"');
 include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
 
 $action = $page->actions();
