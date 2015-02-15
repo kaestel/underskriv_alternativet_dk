1eec06151bcebf7d5c0fccad281669be5e708f92
kaestel <kaestel@e-types.com>
2015-02-15 20:59:38 +0000

    updated after test

--- src/www/vaelgererklaering.php
+++ src/www/vaelgererklaering.php
@@ -4,7 +4,6 @@ if(isset($read_access) && $read_access) {
 	return;
 }
 
-session_set_cookie_params(3600, '/', "underskriv.alternativet.dk");
 include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
 
 
