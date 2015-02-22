ed3cdb61ef15132f4a36e57b4da6f189777e50b5
kaestel <kaestel@e-types.com>
2015-02-22 00:05:29 +0000

    updated manipulator

---
+++ src/www/js/lib/seg_desktop_ie_include.js
@@ -0,0 +1,9 @@
+document.write('<script type="text/javascript" src="/js/manipulator/v0_9/full/seg_desktop_ie.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/u-form-custom.js"></script>');
+
+
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-page-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-declaration-desktop.js"></script>');
+
+document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/v0_9/src/u-googleanalytics.js"></script>');
