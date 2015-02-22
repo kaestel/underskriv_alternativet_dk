6fe3e4a6319cdf279ce6569706ee23b3f5312175
kaestel <kaestel@e-types.com>
2015-02-22 00:03:39 +0000

    updated manipulator

--- src/www/js/lib/seg_mobile_touch_include.js
+++ src/www/js/lib/seg_mobile_touch_include.js
@@ -1,15 +1,9 @@
 document.write('<script type="text/javascript" src="/js/manipulator/v0_9/full/seg_mobile_touch.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/u-form-custom.js"></script>');
 
-// document.write('<script type="text/javascript" src="/js/manipulator/src/u-animation.js"></script>');
-// document.write('<script type="text/javascript" src="/js/manipulator/src/u-geometry.js"></script>');
-// document.write('<script type="text/javascript" src="/js/manipulator/src/u-events.js"></script>');
-// document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-movements.js"></script>');
-// document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-browser.js"></script>');
-// document.write('<script type="text/javascript" src="/js/manipulator/src/u-system.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/mobile_touch/i-page-mobile_touch.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/mobile_touch/i-declaration-mobile_touch.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/v0_8/src/u-googleanalytics.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/v0_9/src/u-googleanalytics.js"></script>');
